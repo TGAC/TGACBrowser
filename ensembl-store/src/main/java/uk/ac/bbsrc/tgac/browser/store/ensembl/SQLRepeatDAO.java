b7bbaa434f107688d8d2d0b1668686cd9e3dfc32
Anil Thanki <Anil.thanki@tgac.ac.uk>
2013-11-02 20:05:44 +0000

    file bug fixed.. unwanted repeated methods removed

--- ensembl-store/src/main/java/uk/ac/bbsrc/tgac/browser/store/ensembl/SQLRepeatDAO.java
+++ ensembl-store/src/main/java/uk/ac/bbsrc/tgac/browser/store/ensembl/SQLRepeatDAO.java
@@ -67,93 +67,13 @@ public class SQLRepeatDAO implements RepeatStore {
         return template;
     }
 
-    static String var1;
-    public static final String GET_DISPLAYABLE_FROM_ANALYSIS_ID = "SELECT displayable FROM analysis_description where analysis_id =?";
-    public static final String GET_DISPLAYLABLE_FROM_ANALYSIS_ID = "SELECT display_label FROM analysis_description where analysis_id =?";
-    public static final String GET_LOGIC_NAME_FROM_ANALYSIS_ID = "SELECT logic_name FROM analysis where analysis_id =?";
-    public static final String GET_DESCRIPTION_FROM_ANALYSIS_ID = "SELECT description FROM analysis_description where analysis_id =?";
-    public static final String GET_Tracks_API = "select analysis_id, logic_name from analysis";
-    public static final String Get_Tracks_Desc = "select description from analysis_description where analysis_id = ?";
-    public static final String Get_Tracks_Info = "select * from analysis_description";
-    public static final String GET_Tracks_Name = "select analysis_id from analysis where logic_name = ?";
-    public static final String GET_TRACKS_VIEW = "select a.logic_name as name, a.analysis_id as id, ad.description, ad.display_label, ad.displayable, ad.web_data from analysis a, analysis_description ad where a.analysis_id = ad.analysis_id;";
 
-
-    public static final String GET_SEQ_FROM_SEQ_REGION_ID = "SELECT sequence FROM dna WHERE seq_region_id = ?";
-    public static final String GET_SEQ_REGION_ID_FROM_NAME = "SELECT seq_region_id FROM seq_region WHERE name  = ?";
-    public static final String GET_SEQ_REGION_ID_SEARCH = "SELECT s.seq_region_id, s.name, s.length, cs.name as Type FROM seq_region s, coord_system cs WHERE s.name like ? and cs.coord_system_id = s.coord_system_id;";
-//    public static final String GET_SEQ_REGION_ID_SEARCH = "SELECT * FROM seq_region WHERE name like ? limit 100";
-
-    public static final String GET_SIZE_SEQ_REGION_ID_SEARCH = "SELECT count(length) FROM seq_region WHERE name like ?";
-    public static final String GET_SEQ_REGION_ID_SEARCH_all = "SELECT * FROM seq_region WHERE coord_system_id = ?";
-    public static final String GET_SEQ_REGION_NAME_FROM_ID = "SELECT name FROM seq_region WHERE seq_region_id = ?";
-    public static final String GET_SEQ_LENGTH_FROM_ID = "SELECT length FROM seq_region WHERE seq_region_id = ?";
     public static final String GET_length_from_seqreg_id = "SELECT length FROM seq_region where seq_region_id =?";
-    public static final String GET_SEQ_REGION_ID_SEARCH_For_One = "SELECT seq_region_id FROM seq_region WHERE name = ?";
-
-    public static final String GET_SEQ_REGION_ATTRIB_FROM_ID = "SELECT * FROM seq_region_attrib WHERE seq_region_id = ? and attrib_type_id = 3";
-
-
-    public static final String GET_GENE_SEARCH = "SELECT * FROM gene WHERE description like ?";
-    public static final String GET_TRANSCRIPT_SEARCH = "SELECT * FROM transcript WHERE description like ?";
-    public static final String GET_transcript = "SELECT transcript_id, seq_region_start, seq_region_end,description,seq_region_strand FROM transcript where gene_id =? ORDER BY seq_region_start ASC";
-    //  public static final String GET_transcript = "SELECT * FROM transcript where seq_region_id =? AND analysis_id = ? AND ((seq_region_start > ? AND seq_region_end < ?) OR (seq_region_start < ? AND seq_region_end > ?) OR (seq_region_end > ? AND seq_region_end < ?) OR (seq_region_start > ? AND seq_region_start < ?))";
-    public static final String GET_Genes = "SELECT gene_id,seq_region_start,seq_region_end, description,seq_region_strand FROM gene where seq_region_id =? and analysis_id = ? ";//AND ((seq_region_start > ? AND seq_region_end < ?) OR (seq_region_start < ? AND seq_region_end > ?) OR (seq_region_end > ? AND seq_region_end < ?) OR (seq_region_start > ? AND seq_region_start < ?))";
-    public static final String GET_Gene_Details = "SELECT * FROM gene where gene_id =? and analysis_id = ?";
-    public static final String GET_GO_Gene_Details = "SELECT * FROM gene where gene_id =?";
-    public static final String GET_GO_Transcript_Details = "SELECT * FROM transcript where transcript_id =?";
-    public static final String GET_Gene_SIZE_SLICE = "SELECT COUNT(gene_id) FROM gene where seq_region_id =? and analysis_id = ? and seq_region_start >= ? and seq_region_start <= ?";
-    public static final String GET_EXON = "SELECT seq_region_start,seq_region_end,seq_region_strand FROM exon where seq_region_id =?";
-    public static final String GET_EXON_per_Gene = "SELECT e.exon_id, e.seq_region_start, e.seq_region_end, e.seq_region_strand FROM exon e, exon_transcript et where et.exon_id = e.exon_id and et.transcript_id =  ?";
-    public static final String GET_Domain_per_Gene = "SELECT * FROM transcript_attrib where transcript_id =?";
-    public static final String GET_CDS_start_per_Gene = "SELECT seq_start FROM translation where transcript_id =?";
-    public static final String GET_CDS_end_per_Gene = "SELECT seq_end FROM translation where transcript_id =?";
-    public static final String GET_GO_Genes = "select * from gene_attrib where value like ?";
-    public static final String GET_GO_Transcripts = "select * from transcript_attrib where value like ?";
-    public static final String GET_GENE_SIZE = "SELECT COUNT(gene_id) FROM gene where seq_region_id =? and analysis_id = ?";
-    public static final String GET_Gene_name_from_ID = "SELECT description FROM gene where gene_id =?";
-    public static final String GET_Transcript_name_from_ID = "SELECT description FROM transcript where transcript_id =?";
-    public static final String GET_GO_for_Genes = "select value from gene_attrib where gene_id = ?";
-    public static final String GET_GO_for_Transcripts = "select value from transcript_attrib where transcript_id =  ?";
-
-    public static final String GET_START_END_ANALYSIS_ID_FROM_SEQ_REGION_ID = "SELECT seq_region_start,seq_region_end,analysis_id FROM dna_align_feature where req_region_id =?";
-    public static final String GET_HIT_SIZE = "SELECT COUNT(dna_align_feature_id) FROM dna_align_feature where seq_region_id =? and analysis_id = ?";
-    public static final String GET_HIT_SIZE_SLICE = "SELECT COUNT(dna_align_feature_id) FROM dna_align_feature where seq_region_id =? and analysis_id = ? and seq_region_start >= ? and seq_region_start <= ?";
-    public static final String GET_HIT = "SELECT dna_align_feature_id as id,cast(seq_region_start as signed) as start, cast(seq_region_end as signed) as end,seq_region_strand as strand,hit_start as hitstart, hit_end as hitend, hit_name as 'desc', cigar_line as cigarline FROM dna_align_feature where seq_region_id =? and analysis_id = ? AND ((seq_region_start >= ? AND seq_region_end <= ?) OR (seq_region_start <= ? AND seq_region_end >= ?) OR (seq_region_end >= ? AND seq_region_end <= ?) OR (seq_region_start >= ? AND seq_region_start <= ?)) ORDER BY (end-start) desc"; //seq_region_start ASC";//" AND ((hit_start >= ? AND hit_end <= ?) OR (hit_start <= ? AND hit_end >= ?) OR (hit_end >= ? AND hit_end <= ?) OR (hit_start >= ? AND hit_start <= ?))";
-
-
-    public static final String Get_Database_information = "SELECT meta_key,meta_value from meta";// + var1;
-
-    public static final String GET_Seq_API = "SELECT sequence FROM dna where seq_region_id = ?";
-
-    public static final String GET_reference_for_Assembly = "SELECT * FROM assembly where cmp_seq_region_id =?";
     public static final String GET_Assembly_for_reference = "SELECT * FROM assembly where asm_seq_region_id =?";
-    public static final String GET_ASSEMBLY_SIZE_SLICE = "SELECT count(a.asm_seq_region_id) FROM assembly a, seq_region s where a.asm_seq_region_id = ? and a.cmp_seq_region_id = s.seq_region_id and s.coord_system_id = ? and a.asm_start >= ? and a.asm_start <= ?";
-    public static final String GET_ASSEMBLY_SIZE = "SELECT COUNT(asm_seq_region_id) FROM assembly where asm_seq_region_id =? and analysis_id = ? and seq_region_start >= ? and seq_region_start <= ?";
-    public static final String GET_SEQS_LIST_API = "SELECT *  FROM assembly a, seq_region s, coord_system cs  where a.asm_seq_region_id = ? AND s.seq_region_id = a.cmp_seq_region_id AND cs.coord_system_id = s.coord_system_id AND cs.attrib like '%sequence%' AND   ((a.asm_start >= ? AND a.asm_end <= ?) OR (a.asm_start <= ? AND a.asm_end >= ?) OR (a.asm_end >= ? AND a.asm_end <= ?) OR (a.asm_start >= ? AND a.asm_start <= ?))";
-
-
-    public static final String GET_Coords_sys_API = "SELECT coord_system_id,name,rank FROM coord_system where rank > ?";
-    public static final String GET_coord_attrib = "SELECT attrib FROM coord_system where coord_system_id =?";
-    public static final String GET_coord_sys_id = "SELECT coord_system_id FROM seq_region where seq_region_id =?";
-    public static final String GET_coord_sys_name = "SELECT name FROM coord_system where coord_system_id =?";
-    public static final String GET_coord_sys_id_by_name = "SELECT coord_system_id FROM seq_region where name =?";
-    public static final String GET_Coord_systemid_FROM_ID = "SELECT coord_system_id FROM seq_region WHERE seq_region_id = ?";
-    public static final String GET_RANK_for_COORD_SYSTEM_ID = "SELECT rank FROM coord_system where coord_system_id =?";
-    public static final String CHECK_Coord_sys_attr = "select * from coord_system where coord_system_id = ? and (name like ? OR attrib like ?);";
-    public static final String GET_hit_name_from_ID = "SELECT hit_name FROM dna_align_feature where dna_align_feature_id =?";
-    public static final String GET_Gene_by_view = "select g.gene_id, g.seq_region_start as gene_start, g.seq_region_end as gene_end, g.seq_region_strand as gene_strand, g. description as gene_name, t.transcript_id, t.seq_region_start as transcript_start, t.seq_region_end as transcript_end, t.description as transcript_name, e.exon_id, e.seq_region_start as exon_start, e.seq_region_end as exon_end from gene g, transcript t, exon_transcript et, exon e where t.gene_id = g.gene_id and t.transcript_id = et.transcript_id and et.exon_id = e.exon_id and  g.seq_region_id = ? and g.analysis_id = ?;";//"select * from gene_view where seq_region_id = ? and analysis_id = ?;";//
-    public static final String GET_Assembly = "SELECT a.asm_seq_region_id,a.cmp_seq_region_id,a.asm_start,a.asm_end FROM assembly a, seq_region s where a.asm_seq_region_id =? and a.cmp_seq_region_id = s.seq_region_id and s.coord_system_id = ? ORDER BY asm_start ASC";
     public static final String GET_REPEAT = "SELECT repeat_feature_id as id,seq_region_start as start, seq_region_end as end,seq_region_strand as strand, repeat_start as repeatstart,repeat_end as repeatend, score as score FROM repeat_feature where seq_region_id =? and analysis_id = ? AND ((seq_region_start > ? AND seq_region_end < ?) OR (seq_region_start < ? AND seq_region_end > ?) OR (seq_region_end > ? AND seq_region_end < ?) OR (seq_region_start > ? AND seq_region_start < ?)) ORDER BY start,(end-start) asc"; //seq_region_start ASC";//" AND ((hit_start >= ? AND hit_end <= ?) OR (hit_start <= ? AND hit_end >= ?) OR (hit_end >= ? AND hit_end <= ?) OR (hit_start >= ? AND hit_start <= ?))";
     public static final String GET_REPEAT_SIZE = "SELECT COUNT(repeat_feature_id) FROM repeat_feature where seq_region_id =? and analysis_id = ?";
     public static final String GET_REPEAT_SIZE_SLICE = "SELECT COUNT(repeat_feature_id) FROM repeat_feature where seq_region_id =? and analysis_id = ? and seq_region_start >= ? and seq_region_start <= ?";
-    public static final String GET_coord_attrib_chr = "SELECT coord_system_id FROM coord_system where name like ? || attrib like ?";
 
-    //  public static final String GET_GENOME_MARKER = "SELECT * from marker_feature";
-    public static final String GET_GENOME_MARKER = "select mf.marker_feature_id as id, sr.name as reference, mf.marker_id as marker_id, mf.seq_region_start as start, mf.seq_region_end as end, mf.analysis_id as analysis_id from marker_feature mf, seq_region sr where mf.seq_region_id = sr.seq_region_id;";
-
-    public static final String GET_Tables_with_analysis_id_column = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME IN ('analysis_id') AND TABLE_SCHEMA='wrightj_brachypodium_distachyon_core_10_63_12'";
-    public static final String Check_feature_available = "SELECT DISTINCT analysis_id from ";// + var1;
     private JdbcTemplate template;
 
     public void setJdbcTemplate(JdbcTemplate template) {
@@ -386,40 +306,6 @@ public class SQLRepeatDAO implements RepeatStore {
     }
 
 
-    public JSONArray getRepeatLevel(List<Map<String, Object>> maps, List<Integer> ends, long start, long end, int delta) {
-        JSONArray trackList = new JSONArray();
-        for (Map<String, Object> map : maps) {
-            int start_pos = Integer.parseInt(map.get("start").toString());
-            int end_pos = Integer.parseInt(map.get("end").toString());
-            if (start_pos >= start && end_pos <= end || start_pos <= start && end_pos >= end || end_pos >= start && end_pos <= end || start_pos >= start && start_pos <= end) {
-
-                for (int i = 0; i < ends.size(); i++) {
-                    if (start_pos - ends.get(i) > delta) {
-                        ends.remove(i);
-                        ends.add(i, end_pos);
-                        map.put("layer", i + 1);
-                        break;
-                    } else if ((start_pos - ends.get(i) <= delta && (i + 1) == ends.size()) || start_pos == ends.get(i)) {
-
-                        if (i == 0) {
-                            map.put("layer", ends.size());
-                            ends.add(i, Integer.parseInt(map.get("end").toString()));
-                        } else {
-                            ends.add(ends.size(), Integer.parseInt(map.get("end").toString()));
-                            map.put("layer", ends.size());
-                        }
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-
-                trackList.add(map);
-            }
-        }
-        return trackList;
-    }
-
     public JSONArray processRepeat(List<Map<String, Object>> maps, long start, long end, int delta, int id, String trackId) throws IOException {
         try {
             JSONArray trackList = new JSONArray();
@@ -430,7 +316,7 @@ public class SQLRepeatDAO implements RepeatStore {
             if (template.queryForObject(GET_REPEAT_SIZE, new Object[]{id, trackId}, Integer.class) > 0) {
                 if (maps.size() > 0) {
                     ends.add(0, 0);
-                    trackList = getRepeatLevel(maps, ends, start, end, delta);
+                    trackList = getRepeatLevel(0, maps, start, end, delta);
 
                 }
             } else {
@@ -446,9 +332,4 @@ public class SQLRepeatDAO implements RepeatStore {
             throw new IOException("getHit no result found");
         }
     }
-
-
-
-
-
 }
