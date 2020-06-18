7cfe06c7e08306bf5f6c6f4d2b23ccc4a0f448ff
Anil Thanki (EI) <thankia@n140518.nbi.ac.uk>
2020-06-18 17:25:29 +0100

    changes BLAST track div as wrapper format

--- browser-web/src/main/webapp/scripts/blast_params.js
+++ browser-web/src/main/webapp/scripts/blast_params.js
@@ -60,26 +60,28 @@ function setBLASTPenalty(){
     var blosum50 = [[13,3],[12,3],[11,3],[10,3],[9,3],[16,2],[15,2],[14,2],[13,2],[12,2],[19,1],[18,1],[17,1],[16,1],[15,1]];
     var blosum90 = [[9,2],[8,2],[7,2],[6,2],[11,1],[10,1],[9,1]];
 
-    if (jQuery('#matrix').val() == "PAM30") {
-        gen_penalties(pam30)
-    }
-    else if (jQuery('#matrix').val() == "PAM70") {
-        gen_penalties(pam70)
-    }   else if (jQuery('#matrix').val() == "PAM250") {
-        gen_penalties(pam250)
-    }else if (jQuery('#matrix').val() == "BLOSUM80") {
-        gen_penalties(blosum80)
-    }else if (jQuery('#matrix').val() == "bLOSUM45") {
-        gen_penalties(blosum45)
-    }else if (jQuery('#matrix').val() == "BLOSUM62") {
-        gen_penalties(blosum62)
-    }else if (jQuery('#matrix').val() == "BLOSUM50") {
-        gen_penalties(blosum50)
-    }else if (jQuery('#matrix').val() == "BLOSUM90") {
-        gen_penalties(blosum90)
-    }
-    else  {
-        alert("else")
+    if (jQuery('#matrix').val()){
+        if (jQuery('#matrix').val() == "PAM30") {
+            gen_penalties(pam30)
+        }
+        else if (jQuery('#matrix').val() == "PAM70") {
+            gen_penalties(pam70)
+        }   else if (jQuery('#matrix').val() == "PAM250") {
+            gen_penalties(pam250)
+        }else if (jQuery('#matrix').val() == "BLOSUM80") {
+            gen_penalties(blosum80)
+        }else if (jQuery('#matrix').val() == "bLOSUM45") {
+            gen_penalties(blosum45)
+        }else if (jQuery('#matrix').val() == "BLOSUM62") {
+            gen_penalties(blosum62)
+        }else if (jQuery('#matrix').val() == "BLOSUM50") {
+            gen_penalties(blosum50)
+        }else if (jQuery('#matrix').val() == "BLOSUM90") {
+            gen_penalties(blosum90)
+        }
+        else  {
+            alert("else")
+        }
     }
 }
 
@@ -89,7 +91,7 @@ function gen_penalties(penalties){
         penalty_string += "<option value='"+penalties[i]+"'> Existence: "+penalties[i][0]+" Extension: "+penalties[i][1]+"</option>";
     }
     penalty_string += "</select>";
-        jQuery("#penalty_div").html(penalty_string)
+    jQuery("#penalty_div").html(penalty_string)
 }
 
 function setWordSize(){
@@ -157,7 +159,20 @@ function setBLASTTrack(){
 
     jQuery("#BLASTmergegroup-table").append("<div id=blasttrackspan> <input type=\"checkbox\" id='blasttrackmergedCheckbox' name='blasttrackmergedCheckbox' onClick=mergeTrack(\"blasttrack\"); value=blasttrack >Blast Track</div>");
 
-    jQuery("#tracks").append("<div id='blasttrack_div' class='feature_tracks'> Blast Track </div>");
+    jQuery("#tracks").append("<div id='blasttrack_wrapper' class='feature_tracks ui-resizable' style='max-height:110px; overflow: hidden;'>");
+    // jQuery("#tracks").append("<div id='blasttrack_div' class='feature_tracks'> Blast Track </div>");
+
+    jQuery("#blasttrack_wrapper").append("<div align='left' class='handle'>" +
+        "<table>" +
+        "<tr>" +
+        "<td><b>Blast</b></td>" +
+        "<td><div title=\"Track names\"  class=\"closehandle ui-icon ui-icon-comment\" onclick=toogleLabel(\"blasttrack\");> </div></td>" +
+        "<td><div title=\"Hide Track\" class='closehandle ui-icon ui-icon-close' onclick=removeTrack(\"blasttrack_div\",\"blasttrack\");></div></td>" +
+        "</tr>" +
+        "</table>" +
+        "</div>" +
+        "<div id='blasttrack_div' class='feature_tracks' style=\"display:block; top:10px; overflow-y: auto; overflow-x: hidden\" > </div>"
+    );
 
     jQuery("#blasttrack_div").html("<img style='position: relative; left: 50%; ' src='./images/browser/loading_big.gif' alt='Loading'>")
 
