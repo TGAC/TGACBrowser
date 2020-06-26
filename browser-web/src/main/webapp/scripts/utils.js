d13515db502abe66b7887f8b9eb887168e80c630
Anil Thanki (EI) <thankia@n140518.nbi.ac.uk>
2020-06-26 13:13:07 +0100

    adds copy to clipboard functionality for fasta popup

--- browser-web/src/main/webapp/scripts/utils.js
+++ browser-web/src/main/webapp/scripts/utils.js
@@ -663,3 +663,17 @@ function trackToGraph(div, trackName, className) {
     window['track_list' + trackName].graphType = "bar"
 
 }
+
+function CopyToClipboard(containerid) {
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select().createTextRange();
+        document.execCommand("copy");
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+    }
+}
